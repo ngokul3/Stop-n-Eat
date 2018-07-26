193d70494687d355d9ac887459c2b1d1be815168
ngokul3 <ngokul3@gmail.com>
2018-07-25 20:15:55 -0400

    changes to storyboard

--- Assignment5/Assignment5/ViewController.swift
+++ Assignment5/Assignment5/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    var sourceArr = ["Hoboken Train Station","Millburn","Newark","Short Hills"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return sourceArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //
+        //        if let menuItems = menuItemsInBill {
+        //            let arr = menuItems
+        //            let row = indexPath.row
+        //            let cell = tableView.dequeueReusableCell(withIdentifier: "foodCell", for: indexPath)
+        //            cell.textLabel?.text = arr[row].itemdescription
+        //            return cell
+        //        }
+        //        else{
+        //            preconditionFailure("Cell Identifier not set")
+        //        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "trainCell", for: indexPath)
+        cell.textLabel?.text = sourceArr[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // print("Table row: \(indexPath.row) was selected")
+      //  tableView.scrollToRow(at: indexPath, at: .middle, animated: true)
     }
 
-
+    
+   
+    
 }
-
