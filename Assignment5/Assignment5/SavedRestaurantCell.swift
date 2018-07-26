09ff88c09aaf38cd3af92f767f36d499f18d24d6
ngokul3 <ngokul3@gmail.com>
2018-07-26 10:34:46 -0400

    changing saved rest

---
+++ Assignment5/Assignment5/SavedRestaurantCell.swift
@@ -0,0 +1,31 @@
+//
+//  SavedRestaurantCellTableViewCell.swift
+//  Assignment5
+//
+//  Created by Gokula K Narasimhan on 7/26/18.
+//  Copyright © 2018 Gokula K Narasimhan. All rights reserved.
+//
+
+import UIKit
+
+class SavedRestaurantCell: UITableViewCell {
+
+    @IBOutlet weak var txtDateSaved: UILabel!
+    @IBOutlet weak var txtNotesRestaurant: UITextView!
+    @IBOutlet weak var imgRail: UIImageView!
+    @IBOutlet weak var imgStar1: UIImageView!
+    @IBOutlet weak var imgStar2: UIImageView!
+    @IBOutlet weak var imgStar3: UIImageView!
+    @IBOutlet weak var imgStar4: UIImageView!
+    @IBOutlet weak var imgStar5: UIImageView!
+    @IBOutlet weak var lblMiles: UILabel!
+    @IBOutlet weak var lblRestaurantName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+   
+
+}
--- Assignment5/Assignment5/SavedRestaurantVC.swift
+++ Assignment5/Assignment5/SavedRestaurantVC.swift
@@ -1,27 +1,30 @@
 //
-//  RestaurantVC.swift
+//  SavedRestaurantVC.swift
 //  Assignment5
 //
-//  Created by Gokula K Narasimhan on 7/25/18.
+//  Created by Gokula K Narasimhan on 7/26/18.
 //  Copyright © 2018 Gokula K Narasimhan. All rights reserved.
 //
 
 import UIKit
 
-class RestaurantVC: UIViewController {
-    
-    var cellArrray = [RestaurantCell]()
-            override func viewDidLoad() {
-            super.viewDidLoad()
+class SavedRestaurantVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
+
+
 }
 
-extension RestaurantVC : UITableViewDataSource{
+extension SavedRestaurantVC : UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       
-        let CellID = "restaurantCell"
         
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: CellID, for: indexPath) as? RestaurantCell else{
+        let CellID = "savedRestaurantCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CellID, for: indexPath) as? SavedRestaurantCell else{
             preconditionFailure("Incorrect cell provided -- see storyboard")
         }
         switch indexPath.row{
@@ -33,22 +36,26 @@ extension RestaurantVC : UITableViewDataSource{
             cell.imgStar1.image = UIImage(named: "Rating.png")
             cell.imgStar2.image = UIImage(named: "Rating.png")
             cell.imgStar3.image = UIImage(named: "Rating.png")
-            cell.imgInfo.image = UIImage(named: "BlueInfo.png")
+            cell.txtNotesRestaurant.text = "Yelp gave 3 R. Service is good"
+            cell.txtDateSaved.text = "07/01/2018"
         case 1:
             cell.lblMiles.text = "3.1 mi"
             cell.lblRestaurantName.text = "Dosa Corner"
             cell.imgRail.image = UIImage(named: "Rail.png")
             cell.imgStar1.image = UIImage(named: "Rating.png")
             cell.imgStar2.image = UIImage(named: "Rating.png")
-            
+             cell.txtNotesRestaurant.text = "Mint Sauce is not spicy enough"
+             cell.txtDateSaved.text = "05/03/2018"
         case 2:
             cell.lblMiles.text = "3 mi"
-            cell.lblRestaurantName.text = "Chipotle"
+            cell.lblRestaurantName.text = "Chinese Bowl"
             cell.imgRail.image = UIImage(named: "Rail")
             cell.imgStar1.image = UIImage(named: "Rating.png")
+             cell.txtNotesRestaurant.text = "Noodles was not enough"
+             cell.txtDateSaved.text = "07/11/2018"
         default : break
         }
-       
+        
         
         
         return cell
@@ -57,5 +64,4 @@ extension RestaurantVC : UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
- }
-
+}
